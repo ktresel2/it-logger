d7ecbfa5c59583801363ff4fd55dfbc2f457f564
ktresel2 <ktreseler2@gmail.com>
2022-03-03 06:55:21 -0500

    Add log component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, Fragment } from 'react'
+import SearchBar from './components/layout/SearchBar'
+import Logs from './components/logs/Logs'
 
 import 'materialize-css/dist/css/materialize.min.css'
 import M from 'materialize-css/dist/js/materialize.min.js'
@@ -8,7 +10,16 @@ const App = () => {
 	useEffect(() => {
 		M.AutoInit()
 	})
-	return <div className="App">App</div>
+	return (
+		<div className="App">
+			<Fragment>
+				<SearchBar />
+				<div className="container">
+					<Logs />
+				</div>
+			</Fragment>
+		</div>
+	)
 }
 
 export default App
